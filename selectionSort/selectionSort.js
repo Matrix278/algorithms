3a7ba8c34849e2fb16ad25accb97ce6bbf7959d5
Martin Sidorov <35573364+Matrix278@users.noreply.github.com>
2019-12-11 23:04:46 +0200

    Update selectionSort.js

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -1,1 +1,18 @@
+function selectionSort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
 
+selectionSort([1, 5, 6, 2, 3, 8, 4, 7, 9,]);
