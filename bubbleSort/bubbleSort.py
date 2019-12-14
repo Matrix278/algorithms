7620808bbb31d5e049abe173fbdddcadfc3bf47e
WagOne <thenikitenko@gmail.com>
2019-12-14 21:31:40 +0200

    Create bubbleSort.py

---
+++ bubbleSort/bubbleSort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arrayOfNumbers):
+    
+    for i in range(len(arrayOfNumbers) - 1):
+
+        for j in range(len(arrayOfNumbers) - 1 - i):
+
+            if arrayOfNumbers[j] > arrayOfNumbers[j + 1]:
+
+                arrayOfNumbers[j], arrayOfNumbers[j + 1] = arrayOfNumbers[j + 1], arrayOfNumbers[j]
+
+    return arrayOfNumbers
+
+print(bubbleSort([5,3,2,4,1]))
