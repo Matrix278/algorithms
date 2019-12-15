dcac91cdb5deb489bb94685bd84ece5f1710d5fb
Martin Sidorov <35573364+Matrix278@users.noreply.github.com>
2019-12-15 13:28:28 +0200

    Update bubbleSort.php

--- bubbleSort/bubbleSort.php
+++ bubbleSort/bubbleSort.php
@@ -1,31 +1,17 @@
 <?php
-
-function bubbleSort($arrayOfNumbers){
-
-    for($i = 0; $i < sizeof($arrayOfNumbers) - 1; $i++){
-
-        for($j = 0; $j < sizeof($arrayOfNumbers) - 1 - $i; $j++){
-
-            $k = $j+1;
-
-            if($arrayOfNumbers[$k] < $arrayOfNumbers[$j]){
-
-                $temporary = $arrayOfNumbers[$k];
-                
-                $arrayOfNumbers[$k] = $arrayOfNumbers[$j];
-
-                $arrayOfNumbers[$j] = $temporary;
-
+   function bubbleSort($arr){
+        for($i = 0; $i < sizeof($arr) - 1; $i++){
+            for($j = 0; $j < sizeof($arr) - 1 - $i; $j++){
+                $k = $j+1;
+                if($arr[$k] < $arr[$j]){
+                    $temp = $arr[$k];
+                    $arr[$k] = $arr[$j];
+                    $arr[$j] = $temp;
+                }
             }
-
         }
-
+        return $arr;
     }
 
-    return $arrayOfNumbers;
-
-}
-
-bubbleSort(array(5,3,2,4,1));
-
+    bubbleSort(array(5,3,2,4,1));
 ?>
