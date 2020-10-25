ace3aeacbdb732f715b1aecff8a9b741d81f7deb
Martin Sidorov <martin.sidorov27@gmail.com>
2020-10-25 17:45:40 +0200

    Update bubbleSort.java

--- bubbleSort/bubbleSort.java
+++ bubbleSort/bubbleSort.java
@@ -5,10 +5,10 @@ public class bubbleSort {
          for(int i=0; i < n; i++){  
              for(int j=1; j < (n-i); j++){  
                   if(arr[j-1] > arr[j]){
-                         //swap elements  
-                         temp = arr[j-1];
-                         arr[j-1] = arr[j];
-                         arr[j] = temp;
+                     //swap elements  
+                     temp = arr[j-1];
+                     arr[j-1] = arr[j];
+                     arr[j] = temp;
                  }
 
              }
